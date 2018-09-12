d5a50e7f6e1e2de1696f62f2790aaa2492d8f192
Rômulo Bourget Novas <romulo.bnovas@gmail.com>
2018-09-11 23:02:10 -0300

    Fixing syntax error on setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 envsubst < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
 for filename in /etc/nginx/conf.d/*.conf; do
-        envsubst < $filename > $filename
-    done
+    envsubst < $filename > $filename
 done
 
 for filename in /etc/nginx/sites-available/*.conf; do
-        envsubst < $filename > $filename
-    done
+    envsubst < $filename > $filename
 done
